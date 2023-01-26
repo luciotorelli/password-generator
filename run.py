d925a8a209c2e3b318e9f2a82621cdc08747b2ae
luciotorelli <luciotorelli@hotmail.com>
2023-01-26 14:06:06 +0000

    Add pyfiglet, colorama and os

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import random
 import string
 import time
+import pyfiglet
+from colorama import Fore, Style
+import os
+
 
 """Password Generator is a Python app that runs on a terminal.
 It creates a password based on the amount of characters requested
@@ -19,13 +23,18 @@ def get_password_length():
     The loop will repeatedly request data, until it is valid.
     """
 
+    # Uses Colorama and PyFiglet to style the text
+    print(Fore.BLUE + Style.DIM + pyfiglet.figlet_format("Passgen"))
+    print(pyfiglet.figlet_format("by Lucio Torelli", font="digital"))
+    # Resets Colorama style
+    print(Style.RESET_ALL)
     print(
-        "Hey there! I am passgen, a very basic bot ",
+        Fore.YELLOW + Style.DIM + "Hey there! I am passgen, a bot",
         "that will generate a password for you.",
     )
     print(
-        "Please enter how many characters you want your",
-        "password to have, select a number between 3 and 100.\n",
+        Fore.YELLOW + Style.DIM + "Enter the number of characters",
+        "you want between 3 and 100 and hit enter.\n",
     )
 
     while True:
@@ -33,22 +42,22 @@ def get_password_length():
             # Requests the password length from the user,
             # Assigns to variable password_length
             # Make input integer
-            password_length = int(input(
-                "Enter your number here and hit enter:\n"))
+            # Make text colored and styled normal for user input
+            password_length = int(input(Fore.WHITE + Style.NORMAL))
 
             # Check if the password is higher than 100
             # or lower than 3, if so, raise ValueError
             if password_length > 100 or password_length < 3:
                 raise ValueError
 
-            # Check if the user selected 1 or
-            # more characters to phrase the result correctly
-            if password_length == 1:
-                print(
-                    f"You selected {password_length} character")
-            else:
-                print(
-                    f"You selected {password_length} characters")
+            # Clears terminal
+            os.system("cls")
+            # Print how many characters user selected.
+            print(
+                Fore.YELLOW
+                + Style.DIM
+                + f"Your password will have {password_length} characters"
+            )
 
             # Returns the password_length
             return password_length
@@ -57,8 +66,8 @@ def get_password_length():
         # higher than 100 or lower than 3 is input by the user
         except ValueError:
             print(
-                "No no no... I said a number between 3 and 100.",
-                "Try again and hit enter."
+                Fore.RED + Style.DIM + "No no no... I said a number between 3 and 100.",
+                "Try again and hit enter.",
             )
 
 
@@ -70,26 +79,19 @@ def get_password_strength():
     The loop will repeatedly request data, until it is valid.
     """
     print(
-        "What level of strength should your password have? ",
-        "For most security I would recommend level 3! :) \n",
+        Fore.YELLOW + Style.DIM + "What level of strength should your password have?",
+        "I would recommend level 3! :) \n",
     )
-    print(
-        "Level 1: Only letters")
-    print(
-        "Level 2: Letters and numbers")
-    print(
-        "Level 3: Letters, numbers and characters")
-
-    print(
-        "You should type a number between 1 to 3.\n")
+    print(Fore.YELLOW + Style.DIM + "Type 1 for Only letters")
+    print(Fore.YELLOW + Style.DIM + "Type 2 for Letters and numbers")
+    print(Fore.YELLOW + Style.DIM + "Type 3 for Letters, numbers and characters\n")
 
     while True:
         try:
             # Requests the password strength from the user,
             # Assigns to variable password_strength
             # Make input integer
-            password_strength = int(input(
-                "Enter your level here:\n"))
+            password_strength = int(input(Fore.WHITE + Style.NORMAL))
 
             # Check if the level is higher than 3
             # or lower than 1, if so, raise ValueError
@@ -99,18 +101,30 @@ def get_password_strength():
             # Check strength level selected
             # by user to phrase the result correctly
             if password_strength == 1:
+                # Clears terminal
+                os.system("cls")
                 print(
-                    f"You selected level {password_strength}, ",
+                    Fore.YELLOW
+                    + Style.DIM
+                    + f"You selected level {password_strength}, ",
                     "your password will contain only letters",
                 )
             elif password_strength == 2:
+                # Clears terminal
+                os.system("cls")
                 print(
-                    f"You selected level {password_strength}, ",
+                    Fore.YELLOW
+                    + Style.DIM
+                    + f"You selected level {password_strength}, ",
                     "your password will contain letters and numbers.",
                 )
             else:
+                # Clears terminal
+                os.system("cls")
                 print(
-                    f"You selected level {password_strength}, ",
+                    Fore.YELLOW
+                    + Style.DIM
+                    + f"You selected level {password_strength}, ",
                     "your password will contain letters,",
                     "numbers and characters.",
                 )
@@ -122,8 +136,9 @@ def get_password_strength():
         # higher than 3 or lower than 1 is input by the user
         except ValueError:
             print(
-                "Hey! I said a number between 1 and 3",
-                ", try again and hit enter!")
+                Fore.RED + Style.DIM + "Hey! I said a number between 1 and 3",
+                ", try again and hit enter!",
+            )
 
 
 def generate_password(password_length, password_strength):
@@ -139,9 +154,7 @@ def generate_password(password_length, password_strength):
         # Adds a random letter to the password
         # until the password_length is reached.
         while i < password_length:
-            password = password + \
-                random.choice(
-                    string.ascii_letters)
+            password = password + random.choice(string.ascii_letters)
             i += 1
         return password
 
@@ -154,17 +167,12 @@ def generate_password(password_length, password_strength):
         # If the iterable is even, adds a letter. Else, adds an integer.
         while i < password_length:
             if i % 2:
-                password = password + \
-                    random.choice(
-                        string.ascii_letters)
+                password = password + random.choice(string.ascii_letters)
             else:
-                password = password + \
-                    str(random.randint(
-                        0, 9))
+                password = password + str(random.randint(0, 9))
             i += 1
         # Shuffles the password randomly
-        password = "".join(random.sample(
-            password, len(password)))
+        password = "".join(random.sample(password, len(password)))
 
         return password
 
@@ -174,8 +182,7 @@ def generate_password(password_length, password_strength):
         # Creates a list of special characters,
         # string.punctuation could be used instead
         # if you don't want to specify the special characters.
-        special_characters = (
-            "$", "#", "@", "!", "%", "^", "&", "*")
+        special_characters = ("$", "#", "@", "!", "%", "^", "&", "*")
         i = 0
         # Adds a random letter, integer or character
         # to the password until the password_length is reached.
@@ -184,21 +191,14 @@ def generate_password(password_length, password_strength):
         # Else, adds an integer.
         while i < password_length:
             if i % 2:
-                password = password + \
-                    random.choice(
-                        string.ascii_letters)
+                password = password + random.choice(string.ascii_letters)
             elif i % 3 == 0:
-                password = password + \
-                    random.choice(
-                        special_characters)
+                password = password + random.choice(special_characters)
             else:
-                password = password + \
-                    str(random.randint(
-                        0, 9))
+                password = password + str(random.randint(0, 9))
             i += 1
         # Shuffles the password randomly
-        password = "".join(random.sample(
-            password, len(password)))
+        password = "".join(random.sample(password, len(password)))
 
         return password
 
@@ -207,34 +207,24 @@ def print_password(password):
     """
     Prints the password on the console for the user.
     """
-    print(
-        "Bip bop boop... computing your password...")
-    print(
-        "...")
-    # Pauses the program for 2 seconds then resumes.
-    time.sleep(
-        2)
-    print(
-        "...")
-    # Pauses the program for 4 seconds then resumes.
-    time.sleep(
-        3)
-    print(
-        "I hope you are still there, working hard on this...")
-    print(
-        "...")
-    # Pauses the program for 4 seconds then resumes.
-    time.sleep(
-        4)
+    print(Fore.YELLOW + Style.DIM + "Bip bop boop... computing your password...")
+    print("...")
+    # Pauses the program for 1 second then resumes.
+    time.sleep(1)
+    print("...")
+    # Pauses the program for 1 second then resumes.
+    time.sleep(1)
+    print("I hope you are still there...")
+    print("...")
+    # Pauses the program for 1 second then resumes.
+    time.sleep(1)
 
-    print(
-        "Phew! This was hard. Here is your password:\n")
-    print(
-        "_______________________")
-    print(
-        password)
-    print(
-        "_______________________")
+    print("Phew! This was hard. Here is your password:\n")
+    # Pauses the program for 1 second then resumes.
+    time.sleep(1)
+    print(Fore.YELLOW + Style.BRIGHT + password)
+    # Resets colorama style
+    print(Style.RESET_ALL)
 
 
 def main():
@@ -243,10 +233,8 @@ def main():
     """
     password_length = get_password_length()
     password_strength = get_password_strength()
-    password = generate_password(
-        password_length, password_strength)
-    print_password(
-        password)
+    password = generate_password(password_length, password_strength)
+    print_password(password)
 
 
 main()
