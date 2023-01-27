dd7197f652d4bf89b310be377bd369056a938404
luciotorelli <luciotorelli@hotmail.com>
2023-01-27 16:01:06 +0000

    Add user custom special character list feature

--- run.py
+++ run.py
@@ -28,13 +28,15 @@ def get_password_length():
     # Resets Colorama style
     print(Style.RESET_ALL)
     # Sets text color yellow
-    print(Fore.YELLOW +
+    print(
+        Fore.YELLOW +
         "Hey there! I am passgen, a bot that will generate a password for you."
     )
-    print("You can type exit or restart at any point during the program run.")
+    print("If you want to exit or restart the program"+
+          " you can type exit or restart at any point.")
     print(
-        "Enter the number of characters "
-        + "you want between 3 and 100 and hit enter."
+        "Enter the number of characters " + 
+        "you want between 3 and 100 and hit enter."
     )
 
     while True:
@@ -63,7 +65,9 @@ def get_password_length():
             # Sends Clear Terminal command based on Operating System
             os.system("cls" if os.name == "nt" else "clear")
             # Print how many characters user selected.
-            print(Fore.YELLOW + f"Your password will have {password_length} characters.\n")
+            print(
+                Fore.YELLOW + f"Your password will have {password_length} characters.\n"
+            )
 
             # Returns the password_length
             return password_length
@@ -71,8 +75,9 @@ def get_password_length():
         # Raise ValueError if a character other than a number,
         # higher than 100 or lower than 3 is input by the user
         except ValueError:
-            print(Fore.RED +
-                "No no no... I said a number between 3 and 100. "
+            print(
+                Fore.RED
+                + "No no no... I said a number between 3 and 100. "
                 + "Try again and hit enter.\n"
             )
 
@@ -84,8 +89,9 @@ def get_password_strength():
     via the terminal, which must be an integer between 1 to 3.
     The loop will repeatedly request data, until it is valid.
     """
-    print( Fore.YELLOW +
-        "What level of strength should your password have? "
+    print(
+        Fore.YELLOW
+        + "What level of strength should your password have? "
         + "I would recommend level 3! :) \n",
     )
     print(Fore.YELLOW + "Type 1 for Only letters")
@@ -131,8 +137,106 @@ def get_password_strength():
         # Raise ValueError if a character other than a number,
         # higher than 3 or lower than 1 is input by the user
         except ValueError:
-            print(Fore.RED +
-                "Hey! I said a number between 1 and 3" + ", try again and hit enter!\n"
+            print(
+                Fore.RED
+                + "Hey! I said a number between 1 and 3"
+                + ", try again and hit enter!\n"
+            )
+
+
+def get_special_characters():
+
+    print(
+        Fore.YELLOW
+        + "Please note this list of characters will be used to ",
+        "randomize the password but not all of them may be included in the password."
+    )
+    print(Fore.YELLOW + "Default List: $#@!%^&*")
+    print(
+        Fore.YELLOW
+        + "Would you like to use the default list or type a custom list? Type default or custom."
+    )
+
+    while True:
+        try:
+            # Requests the list type from the user,
+            # Assigns to variable list_type
+            list_type = input(Fore.WHITE)
+
+            # Checks if user wants to restart or exit
+            # else, assigns the type of list the user wants to use
+            if list_type == "exit" or list_type == "close":
+                closeProgram()
+            elif list_type == "restart" or list_type == "reset":
+                restartProgram()
+                break
+            elif list_type == "default" or list_type == "Default":
+                special_characters = ("$", "#", "@", "!", "%", "^", "&", "*")
+            elif list_type == "custom" or list_type == "Custom":
+                special_characters = get_custom_special_characters()
+            else:
+                raise ValueError
+
+            # Returns the special characters
+            return special_characters
+
+        # Raise ValueError if user typed anything other than
+        # custom, default, restart or exit
+        except ValueError:
+            print(
+                Fore.RED
+                + "Not quite! You need to type default or custom"
+                + ", try again and hit enter!\n"
+            )
+
+
+def get_custom_special_characters():
+    print(
+        Fore.YELLOW
+        + "Type your special character list, do not space or separate characters."
+    )
+    print(Fore.YELLOW + "Example: $@!")
+
+    while True:
+        try:
+            # Requests the list of custom special characters
+            # from the user and assigns to variable user_characters
+            user_characters = input(Fore.WHITE)
+            # Assigns every punctuation available on string.punctuation to special_characters
+            special_characters = list(string.punctuation)
+
+            # Checks if user wants to restart or exit
+            if user_characters == "exit" or user_characters == "close":
+                closeProgram()
+                break
+            elif user_characters == "restart" or user_characters == "reset":
+                restartProgram()
+                break
+
+            # Converts to a set to ensure no duplicates 
+            # then convert it back to a list
+            custom_special_characters = list(set(user_characters))
+
+            # Checks if user typed a valid special character then
+            # returns special_characters else, raises ValueError
+            if all(
+                character in special_characters
+                for character in custom_special_characters
+            ):
+                special_characters = custom_special_characters
+                print(
+                    Fore.YELLOW + f"You selected the characters: {''.join(special_characters)}"
+                )
+                return special_characters
+            else:
+                raise ValueError
+
+        # Raise ValueError if user typed anything other than
+        # custom, default, restart or exit
+        except ValueError:
+            print(
+                Fore.RED + "Nope! You need to type special characters such as !@%^",
+                ", try again and hit enter!\n",
             )
 
 
@@ -174,16 +278,15 @@ def generate_password(password_length, password_strength):
     # If user selects strength level 1
     if password_strength == 3:
         password = ""
-        # Creates a list of special characters,
-        # string.punctuation could be used instead
-        # if you don't want to specify the special characters.
-        special_characters = ("$", "#", "@", "!", "%", "^", "&", "*")
+
         i = 0
         # Adds a random letter, integer or character
         # to the password until the password_length is reached.
         # If the iterable is even, adds a letter.
         # If it's divisible by 3, adds a special character.
         # Else, adds an integer.
+
+        special_characters = get_special_characters()
         while i < password_length:
             if i % 2:
                 password = password + random.choice(string.ascii_letters)
@@ -253,6 +356,7 @@ def print_password(password):
                 "Try yes or no and hit enter.",
             )
 
+
 def restartProgram():
     """
     Resets the style, clears the terminal and restart the program
@@ -277,6 +381,11 @@ def main():
     """
     Run all program functions
     """
+    
+    # Sends Clear Terminal command based on Operating System 
+    # before running all functions
+    os.system("cls" if os.name == "nt" else "clear")
+    
     password_length = get_password_length()
     password_strength = get_password_strength()
     password = generate_password(password_length, password_strength)
