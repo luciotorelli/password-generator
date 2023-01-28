1dfcfbe954e00e048bc1d5a5db8ded64dbe32aee
luciotorelli <luciotorelli@hotmail.com>
2023-01-28 15:05:14 +0000

    Improve docstring, comments and grammar.

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
+"""
+Imports module
+    random
+        this module implements pseudo-random number generators
+        for various distributions. It is used in this project to
+        generate random integers, strings and characters.
+    string
+        Python String module contains some constants,
+        utility function and classes for string manipulation.
+        It is used in this project to manipulate the password.
+    time
+        this module provides various time-related functions.
+        It is used in this project to sleep the program while
+        the bot is typing the final messages.
+    pyfiglet
+        pyfiglet is a full port of FIGlet into pure python.
+        It takes ASCII text and renders it in ASCII art fonts. It is used in
+        this project to display the first message.
+    colorama
+        Makes ANSI escape character sequences
+        (for producing colored terminal text and cursor positioning).
+        It is used in this project to differentiate errors, bot messages
+        and user input.
+    os
+        This module provides a portable way of using
+        operating system dependent functionality.
+        It is used in this project to clear the terminal in-between functions.
+"""
+
 import random
 import string
 import time
@@ -6,50 +35,46 @@ from colorama import Fore, Style
 import os
 import sys
 
-"""Password Generator is a Python app that runs on a terminal.
-It creates a password based on the amount of characters requested
-by the user and a password strength of 3 levels.
-1 Letters only.
-2 Letters and numbers.
-3 Letters, numbers and special characters.
-"""
-
 
 def get_password_length():
-    """
-    Get password length input from the user.
-    Run a while loop to collect a valid data from the user
-    via the terminal, which must be an integer between 3 to 100.
-    The loop will repeatedly request data, until it is valid.
+    """Gets password length from a user input.
+
+    Runs a while loop to collect valid data from the user
+    via the terminal, which must be an integer between 3 and 100.
+    The loop will repeatedly request data until it is valid.
+
+    Returns
+    -------
+    password_length
+        the length the password will have as an integer number.
     """
 
-    # Uses Colorama and PyFiglet to style the text
+    # Uses Colorama and PyFiglet to style the app logo
     print(Fore.BLUE + pyfiglet.figlet_format("Passgen"))
     print(pyfiglet.figlet_format("by Lucio Torelli", font="digital"))
     # Resets Colorama style
     print(Style.RESET_ALL)
-    # Sets text color yellow
+    # Sets text color yellow and prints a welcome message
     print(
         Fore.YELLOW
-        + "Hey there! I am passgen, a bot that will generate a password for you."
+        + "Hey there! I am passgen, a bot that will"
+        + " generate a password for you."
     )
     print(
         "If you want to exit or restart the program"
         + " you can type exit or restart\nat any point."
     )
     print(
-        "Enter the number of characters " + "you want between 3 and 100 and hit enter."
+        "Enter the number of characters "
+        + "you want between 3 and 100 and hit enter."
     )
 
     while True:
         try:
-            # Requests the password length from the user,
-            # Assigns to variable password_length
-            # Make text colored white for user input
             password_length = input(Fore.WHITE)
 
             # Checks if user wants to restart or exit
-            # else, assigns the user input to password_length
+            # else, assigns the user input to password_length as an integer
             if password_length == "restart" or password_length == "reset":
                 restart_program()
                 break
@@ -59,23 +84,18 @@ def get_password_length():
             else:
                 password_length = int(password_length)
 
-            # Check if the password is higher than 100
-            # or lower than 3, if so, raise ValueError
             if password_length > 100 or password_length < 3:
                 raise ValueError
 
-            # Sends Clear Terminal command based on Operating System
+            # Sends clear terminal command based on Operating System
             os.system("cls" if os.name == "nt" else "clear")
-            # Print how many characters user selected.
             print(
-                Fore.YELLOW + f"Your password will have {password_length} characters.\n"
+                Fore.YELLOW
+                + f"Your password will have {password_length} characters.\n"
             )
 
-            # Returns the password_length
             return password_length
 
-        # Raise ValueError if a character other than a number,
-        # higher than 100 or lower than 3 is input by the user
         except ValueError:
             print(
                 Fore.RED
@@ -85,59 +105,61 @@ def get_password_length():
 
 
 def get_password_strength():
+    """Gets password length from a user input.
+
+    Runs a while loop to collect valid data from the user
+    via the terminal, which must be an integer between 1 and 3.
+    The loop will repeatedly request data until it is valid.
+
+    Returns
+    -------
+    password_strength
+        the strength level the password will have as an integer number.
     """
-    Get password strength input from the user.
-    Run a while loop to collect a valid data from the user
-    via the terminal, which must be an integer between 1 to 3.
-    The loop will repeatedly request data, until it is valid.
-    """
+
     print(
         Fore.YELLOW
         + "What level of strength should your password have? "
-        + "I would recommend level 3! :) \n",
+        + "I would recommend level 3! :)\n",
     )
-    print(Fore.YELLOW + "Type 1 for Only letters")
-    print(Fore.YELLOW + "Type 2 for Letters and numbers")
-    print(Fore.YELLOW + "Type 3 for Letters, numbers and characters\n")
+    print(Fore.YELLOW + "Type 1 for letters only.")
+    print(Fore.YELLOW + "Type 2 for letters and numbers.")
+    print(Fore.YELLOW + "Type 3 for letters, numbers and characters.\n")
 
     while True:
         try:
-            # Requests the password strength from the user,
-            # Assigns to variable password_strength
             password_strength = input(Fore.WHITE)
 
             # Checks if user wants to restart or exit
-            # else, assigns the user input to password_strength as integer
+            # else, assigns the user input to password_strength as an integer
             if password_strength == "exit" or password_strength == "close":
                 close_program()
                 break
-            elif password_strength == "restart" or password_strength == "reset":
+            elif (
+                password_strength == "restart" or password_strength == "reset"
+            ):
                 restart_program()
                 break
             else:
                 password_strength = int(password_strength)
 
-            # Check if the level is higher than 3
-            # or lower than 1, if so, raise ValueError
             if password_strength > 3 or password_strength < 1:
                 raise ValueError
 
-            # Check strength level selected
-            # by user to phrase the result correctly
             if (
                 password_strength == 1
                 or password_strength == 2
                 or password_strength == 3
             ):
-                # Sends Clear Terminal command based on Operating System
+                # Sends clear terminal command based on Operating System
                 os.system("cls" if os.name == "nt" else "clear")
-                print(Fore.YELLOW + f"You selected level {password_strength}\n")
+                print(
+                    Fore.YELLOW
+                    + f"You selected strength level {password_strength}.\n"
+                )
 
-            # Returns the password_strength
             return password_strength
 
-        # Raise ValueError if a character other than a number,
-        # higher than 3 or lower than 1 is input by the user
         except ValueError:
             print(
                 Fore.RED
@@ -147,10 +169,23 @@ def get_password_strength():
 
 
 def get_special_characters():
+    """Gets a list of special characters.
+
+    Runs a while loop to collect valid data from the user
+    via the terminal, which must be custom or default.
+    The loop will repeatedly request data until it is valid.
+
+    Returns
+    -------
+    special_characters
+        a list of special characters to be considered when randomizing
+        the password.
+    """
 
     print(
         Fore.YELLOW + "Please note this list of characters will be used to",
-        "randomize the password\nbut not all of them may be included in the password.",
+        "randomize the password\n"
+        + "but not all of them may be included in the password.",
     )
     print(Fore.YELLOW + "Default List: $#@!%^&*")
     print(
@@ -161,12 +196,11 @@ def get_special_characters():
 
     while True:
         try:
-            # Requests the list type from the user,
-            # Assigns to variable list_type
             list_type = input(Fore.WHITE)
 
-            # Checks if user wants to restart or exit
-            # else, assigns the type of list the user wants to use
+            # Checks if user wants to restart, exit
+            # or to assign a list type of custom or default
+            # else, raises ValueError
             if list_type == "exit" or list_type == "close":
                 close_program()
             elif list_type == "restart" or list_type == "reset":
@@ -179,11 +213,8 @@ def get_special_characters():
             else:
                 raise ValueError
 
-            # Returns the special characters
             return special_characters
 
-        # Raise ValueError if user typed anything other than
-        # custom, default, restart or exit
         except ValueError:
             print(
                 Fore.RED
@@ -193,18 +224,31 @@ def get_special_characters():
 
 
 def get_custom_special_characters():
+    """Gets a list of custom special characters.
+
+    Runs a while loop to collect valid data from the user
+    via the terminal, which must be a valid special character list.
+    The loop will repeatedly request data until it is valid.
+
+    Returns
+    -------
+    special_characters
+        a list of special characters to be considered when randomizing
+        the password.
+    """
+
     print(
         Fore.YELLOW
-        + "Type your special character list, do not space or separate characters."
+        + "Type your special character list, "
+        + "do not space or separate characters."
     )
     print(Fore.YELLOW + "Example: $@!")
 
     while True:
         try:
-            # Requests the list of custom special characters
-            # from the user and assigns to variable user_characters
             user_characters = input(Fore.WHITE)
-            # Assigns every punctuation available on string.punctuation to special_characters
+            # Assigns every punctuation available
+            # on string.punctuation to special_characters as a list
             special_characters = list(string.punctuation)
 
             # Checks if user wants to restart or exit
@@ -216,10 +260,10 @@ def get_custom_special_characters():
                 break
 
             # Converts to a set to ensure no duplicates
-            # then convert it back to a list
+            # then converts it to a list
             custom_special_characters = list(set(user_characters))
 
-            # Checks if user typed a valid special character then
+            # Checks if user typed a valid special character list then
             # returns special_characters else, raises ValueError
             if all(
                 character in special_characters
@@ -228,28 +272,42 @@ def get_custom_special_characters():
                 special_characters = custom_special_characters
                 print(
                     Fore.YELLOW
-                    + f"You selected the characters: {''.join(special_characters)}"
+                    + f"You selected the characters: "
+                    + ("".join(special_characters))
                 )
                 return special_characters
             else:
                 raise ValueError
 
-        # Raise ValueError if user typed anything other than
-        # custom, default, restart or exit
         except ValueError:
             print(
-                Fore.RED + "Nope! You need to type special characters such as !@%^",
+                Fore.RED
+                + "Nope! You need to type special characters such as !@%^",
                 ", try again and hit enter!\n",
             )
 
 
 def generate_password(password_length, password_strength):
-    """
-    Generates a password based on the
-    length and strength requested by the user.
+    """Generates a password
+
+    Uses the requested password_length
+    and password_strength to generate a randomized password.
+    If password_strength level 3 is selected, it will also use
+    a list of special characters.
+
+    Parameters
+    ----------
+    password_length : integer
+        the length the password will have as an integer number.
+    password_strength : integer
+        the strength level the password will have as an integer number.
+
+    Returns
+    -------
+    password
+        a randomized password based on the user requirements.
     """
 
-    # If user selects strength level 1
     if password_strength == 1:
         password = ""
         i = 0
@@ -260,13 +318,12 @@ def generate_password(password_length, password_strength):
             i += 1
         return password
 
-    # If user selects strength level 2
     if password_strength == 2:
         password = ""
         i = 0
-        # Adds a random letter or a random integer
-        # to the password until the password_length is reached.
-        # If the iterable is even, adds a letter. Else, adds an integer.
+        # Until the password_length is reached
+        # adds a letter to the password if the iterable is even.
+        # Else, adds an integer to the password.
         while i < password_length:
             if i % 2:
                 password = password + random.choice(string.ascii_letters)
@@ -278,7 +335,6 @@ def generate_password(password_length, password_strength):
 
         return password
 
-    # If user selects strength level 1
     if password_strength == 3:
         password = ""
 
@@ -305,10 +361,15 @@ def generate_password(password_length, password_strength):
 
 
 def print_password(password):
+    """Prints the password to the console.
+
+    Parameters
+    ----------
+    password : string
+        a randomized password based on the user requirements.
     """
-    Prints the password on the console for the user.
-    """
-    print(Fore.YELLOW + "Bip bop boop... computing your password\n")
+
+    print(Fore.YELLOW + "Bip bop boop... computing your password.\n")
     time.sleep(0.2)
     print("...\n")
     print("I hope you are still there...\n")
@@ -318,19 +379,16 @@ def print_password(password):
     print("Phew! This was hard. Here is your password:\n")
 
     print(Fore.YELLOW + Style.BRIGHT)
-    print(password)
+    print(type(password))
 
     print(Fore.YELLOW + Style.NORMAL)
     print("\nWould you like to restart the program? Type yes or no.\n")
 
     while True:
         try:
-            # Requests user decision to stop the program or,
-            # to restart it.
-            # Make text colored white for user input
             restart = input(Fore.WHITE)
 
-            # Check if the user decided to restart
+            # Check if the user wants to restart
             # or to close the program
             if (
                 restart == "yes"
@@ -352,8 +410,6 @@ def print_password(password):
             # Returns to stop while loop
             return
 
-        # If the user inputs anything other than
-        # yes or no, prints error.
         except ValueError:
             print(
                 Fore.RED + "I can't recognize this command.",
@@ -362,30 +418,36 @@ def print_password(password):
 
 
 def restart_program():
+    """Restarts the program
+
+    Resets the style and
+    clears the terminal before restarting
+    the program.
     """
-    Resets the style, clears the terminal and restart the program
-    """
+
     print(Style.RESET_ALL)
     os.system("cls" if os.name == "nt" else "clear")
     os.system('python3 "run.py"')
 
 
 def close_program():
+    """Closes the program
+
+    Sets terminal to yellow, prints a warning message,
+    clears the terminal and closes the program.
     """
-    Sets terminal to Yellow, prints a warning message,
-    clear the terminal and close the program
-    """
+
     os.system("cls" if os.name == "nt" else "clear")
     print(Fore.YELLOW + "\nThe program is now closed.")
     sys.exit()
 
+
 def main():
+    """Runs all program functions
+
+    Sends clear terminal command based on Operating System
+    before running all functions.
     """
-    Run all program functions
-    """
-    # Sends Clear Terminal command based on Operating System
-    # before running all functions
-    # os.system("cls" if os.name == "nt" else "clear")
 
     os.system("cls" if os.name == "nt" else "clear")
 
